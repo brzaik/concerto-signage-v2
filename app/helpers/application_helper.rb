e254553f02e6a50dca36ace4783e64a27199e7da
Brian R Zaik <brian.r.zaik@gmail.com>
2011-09-11 13:31:35 -0600

    Made cleaner titles and did some other stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  # Generate page titles
+  def yield_for_title(content_sym, default)
+    output = content_for(content_sym)
+    output = "#{ controller.action_name.titleize } - #{ controller.controller_name.titleize }" if output.blank?
+    output
+  end
+  
 end
