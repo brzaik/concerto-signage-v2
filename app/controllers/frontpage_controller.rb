6a86ee8508154d7d1c124a229a262e6071d6ca3e
Brian R Zaik <brian.r.zaik@gmail.com>
2011-09-12 21:31:08 -0600

    Added RSS parser on frontpage for Concerto Blog

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,6 +1,17 @@
+require 'rss'
+
 class FrontpageController < ApplicationController
   def index
     @pages = Page.all
+    
+    begin
+      open('http://concerto-signage.org/blog/?feed=rss2')
+    rescue
+      @found_blog = false
+    else 
+      @found_blog = true
+      @rss = RSS::Parser.parse(open('http://concerto-signage.org/blog/?feed=rss2').read, false)
+    end
   end
 end
 
