cd8d036175740dce27afe7f3eaab98d9ad0dd025
Brian R Zaik <brian.r.zaik@gmail.com>
2011-09-12 21:44:02 -0600

    Added truncated post text to display on frontpage

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -4,13 +4,14 @@ class FrontpageController < ApplicationController
   def index
     @pages = Page.all
     
+    blog_url = 'http://www.concerto-signage.org/blog/?feed=rss2'
     begin
-      open('http://concerto-signage.org/blog/?feed=rss2')
+      open(blog_url)
     rescue
       @found_blog = false
     else 
       @found_blog = true
-      @rss = RSS::Parser.parse(open('http://concerto-signage.org/blog/?feed=rss2').read, false)
+      @rss = RSS::Parser.parse(open(blog_url).read, false)
     end
   end
 end
