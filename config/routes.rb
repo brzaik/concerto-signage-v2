0c70dc722ba45c1aa585e4fd6927d56312e46cd7
Brian Michalski <bmichalski@gmail.com>
2011-09-18 21:04:13 -0400

    No tabs in the route file plz.
    Removed all the \t's and replaced them with double spaced for readability.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 ConcOrg2::Application.routes.draw do
   root :to => "frontpage#index"
 	
-	# named routes for major site pages:
-	match "deploy", :to => "pages#show", :id => "deploy"
-	match "demo", :to => "pages#show", :id => "demo"
-	match "overview", :to => "pages#show", :id => "overview"
-	match "why-concerto", :to => "pages#show", :id => "why-concerto"
+  # named routes for major site pages:
+  match "deploy", :to => "pages#show", :id => "deploy"
+  match "demo", :to => "pages#show", :id => "demo"
+  match "overview", :to => "pages#show", :id => "overview"
+  match "why-concerto", :to => "pages#show", :id => "why-concerto"
+  
+  # named routes for auth:
+  match "contact", :to => "contacts#new", :as => "contact"
+  match "login", :to => "user_sessions#new", :as => "login"
+  match "logout", :to => "user_sessions#destroy", :as => "logout"
+  match "register", :to => "users#new", :as => "signup"
 	
-	# named routes for auth:
-	match "contact", :to => "contacts#new", :as => "contact"
-	match "login", :to => "user_sessions#new", :as => "login"
-	match "logout", :to => "user_sessions#destroy", :as => "logout"
-	match "register", :to => "users#new", :as => "signup"
-	
-	match "help", :to => "help_pages#index"
-	match "help_pages/send_email", :to => "help_pages#send_email"  
+  match "help", :to => "help_pages#index"
+  match "help_pages/send_email", :to => "help_pages#send_email"  
   resources :help_pages do
     collection do
       get :search
@@ -24,23 +24,22 @@ ConcOrg2::Application.routes.draw do
 
   resources :categories
   
-	resources :contacts
-	
-	resources :blocks do
-	  member do
-	    get :move_block_higher
-	    get :move_block_lower
-	  end
-	end
-	
-	resources :columns do
-	  resources :blocks
-	  
-	  member do
-	    get :move_column_higher
-	    get :move_column_lower
-	  end
-	end
+  resources :contacts
+  
+  resources :blocks do
+    member do
+      get :move_block_higher
+      get :move_block_lower
+    end
+  end
+  
+  resources :columns do
+  resources :blocks
+    member do
+      get :move_column_higher
+      get :move_column_lower
+    end
+  end
 
   resources :user_sessions
 
@@ -52,9 +51,7 @@ ConcOrg2::Application.routes.draw do
 
   match 'frontpage' => 'frontpage#index'
 
-
   # 301 Redirects for the old site
   match 'view/home', :to => redirect('/')
   match 'view/:slug', :to => redirect('/%{slug}')
-
 end
